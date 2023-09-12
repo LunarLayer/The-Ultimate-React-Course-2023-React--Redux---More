3d989ee79cca956955f7368066d19399f0edc491
TomSteinmeier <113900695+LunarLayer@users.noreply.github.com>
2023-09-12 16:32:41 +0200

    Rename buy-n-split -> 06-eat-n-split + Add unique key to component
    -If a component is re-rendered in the same place as it was before, it's state will persist.
    This state will persist on each re-render, despite some content in the component is changing.
    Providing a unique key to the component on each re-render will ensure that it's unique on every render, and this will in turn, ensure that it's state is reset on every re-render.

--- my-projects/06-eat-n-split/src/App.js
+++ my-projects/06-eat-n-split/src/App.js
@@ -79,6 +79,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
